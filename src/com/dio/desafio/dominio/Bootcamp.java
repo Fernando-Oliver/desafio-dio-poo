62fac84abcd00269428c00a91dd79d932b1b65a7
Fernando de Oliveira Carvalho <fernandooliveiracarvalho98@gmail.com>
2022-07-28 12:09:20 -0300

    Alinhando linhas

--- src/com/dio/desafio/dominio/Bootcamp.java
+++ src/com/dio/desafio/dominio/Bootcamp.java
@@ -35,16 +35,8 @@ public class Bootcamp {
 		return dataInicial;
 	}
 
-	public void setDataInicial(LocalDate dataInicial) {
-		this.dataInicial = dataInicial;
-	}
-
 	public LocalDate getDataFinal() {
 		return dataFinal;
-	}
-
-	public void setDataFinal(LocalDate dataFinal) {
-		this.dataFinal = dataFinal;
 	}
 
 	public Set<Dev> getDevsInscritos() {
